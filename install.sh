4a5eb99ae0e9fdb603c53eb7017ca40a832302bb
JC Chen <ourobolol@gmail.com>
2025-12-04 23:20:47 -0800

    feat: Initial setup of dotfiles and installation script
    - Added `install.sh` to automate dependency checks, Homebrew installation,
      package installation, and config stowing.
    - Added Alacritty configuration with Catppuccin Macchiato theme.
    - Added basic Tmux configuration (`.tmux.conf`).
    - Added Zsh configuration (`.zshrc_stow`) with zplug, fzf, and path settings.
    - Configured `stow` to manage dotfiles for config, zsh, and tmux.

---
+++ install.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+check_dependencies() {
+    echo "checking dependencies..."
+    dependencies=(curl git)
+
+    for d in $dependencies; do
+        command -v $d > /dev/null 2>&1
+        if [[ $? != 0 ]]; then
+            echo "$d required to run install script."
+            exit 1
+        fi
+    done
+    echo "done."
+}
+
+install_brew() {
+    echo "installing homebrew..."
+    if command -v brew >/dev/null 2>&1; then
+        echo "homebrew already installed."
+    else
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+}
+
+install_brew_packages() {
+    echo "installing homebrew packages..."
+    formulae=(tmux zsh stow nvm uv fzf zplug)
+    casks=(alacritty raycast rectangle font-hack-nerd-font visual-studio-code obsidian)
+
+    brew install ${formulae[@]}
+    brew install --cask ${casks[@]}
+    echo "done."
+}
+
+
+install_config() {
+    echo "installing config files..."
+
+    DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+    # TODO: probably move everything that belongs in $HOME into 'home' and just stow the entire folder
+    configs=(config zsh tmux)
+    for cfg in ${configs[@]}; do
+        stow -t "$HOME" -d "$DOTFILES_DIR" "$cfg"
+    done
+
+    # TODO: move extra config files like zsh/autosuggestion-settings.zsh to where the belong
+
+    if ! grep -q "source ~/.zshrc_stow" "$HOME/.zshrc"; then
+        echo "source ~/.zshrc_stow" >> "$HOME/.zshrc"
+    fi
+
+    echo "done."
+}
+
+install() {
+    echo "install start..."
+    check_dependencies
+    install_brew
+    install_brew_packages
+    install_config
+    echo "install done."
+    exit 0
+}
+
+install
